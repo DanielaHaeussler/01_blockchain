6295fdbe4df0f41cd2ec1d783cde46f59b94d6d3
Nelativity <Daniela-Haeussler@gmx.de>
2022-12-23 09:21:06 +0000

    block und dev-test js

---
+++ block.js
@@ -0,0 +1,21 @@
+//Klasse 
+
+class Block{
+
+    constructor(timestamp,lastHash,hash,data){
+        this.timestamp = timestamp; // Zeitstempel
+        this.lastHash = lastHash; // Hash des vorhergehenden Blocks
+        this.hash = hash; // Eigener Hash
+        this.data = data; // Daten
+    }
+
+    toString(){ // für Debugging-Zwecke
+        return `Block -
+        Timestamp:  ${this.timestamp}
+        Last Hash:  ${this.lastHash}
+        Hash:       ${this.hash}
+        Data:       ${this.data}`
+    }
+}
+
+module.exports = Block; // Export al Modul
---
+++ dev-test.js
@@ -0,0 +1,9 @@
+//call: npm run dev-test 
+
+//Import 
+
+const Block = require("./block");
+
+/***Test der block-Funktionalität  */
+
+const block = new Block("aktuelle Zeit","hash1", "hash2", "meineDaten");
