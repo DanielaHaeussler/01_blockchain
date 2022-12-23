dd31b844b883d8bcb7e861aed0449e2ac8ca5b63
Nelativity <Daniela-Haeussler@gmx.de>
2022-12-23 11:33:57 +0000

    update

--- dev-test.js
+++ dev-test.js
@@ -3,6 +3,9 @@
 //Import 
 
 const Block = require("./block");
+const Blockchain = require("./blockchain");
+
+
 
 /***Test der block-Funktionalität  */
 
@@ -12,3 +15,14 @@ console.log(Block.genesis().toString());*/
 
 /*const testBlock = Block.mineBlock(Block.genesis(), "testData");
 console.log(testBlock.toString()); */
+
+/* Test der Blockchain Funktionalität */
+const chain = new Blockchain();
+console.log(chain);
+
+chain.addBlock("Daten von Block 1");
+console.log(chain);
+chain.addBlock("Daten von Block 2");
+console.log(chain);
+chain.addBlock("Daten von Block 3");
+console.log(chain);
