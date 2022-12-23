db6881d9fedc33cfd26ba43ea35976ce6b8c4a71
Nelativity <Daniela-Haeussler@gmx.de>
2022-12-23 11:17:14 +0000

    blockchain

---
+++ blockchain.js
@@ -0,0 +1,17 @@
+// Import
+const Block = require("./block");
+
+class Blockchain{
+constructor(){
+this.chain = [Block.genesis(),];
+
+}
+
+addBlock(blockData){
+const lastBlock = this.chain[this.chain.length -1];
+const block = Block.mineBlock(lastBlock,blockData);
+this.chain.push(block);
+}
+}
+
+module.exports = Blockchain;
